3d2e68f76bbf2fc608405c2a89500d2a3d31cda8
August Lilleaas <august@augustl.com>
2014-01-01 15:00:21 +0100

    Actually doing something.

--- uefi_kernel.c
+++ uefi_kernel.c
@@ -1,5 +1,14 @@
 #include <efi.h>
 
+static CHAR16 *exampleText = L"This is HALT!";
+
 EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab) {
+  UINTN index;
+  EFI_EVENT event = systab->ConIn->WaitForKey;
+  SIMPLE_TEXT_OUTPUT_INTERFACE *conOut = systab->ConOut;
+  conOut->OutputString(conOut, exampleText);
+
+  systab->BootServices->WaitForEvent(1, &event, &index);
+
   return EFI_SUCCESS;
 }
