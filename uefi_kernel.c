77f90cde23f377b960cf802944ec6da2c8db0e48
August Lilleaas <august@augustl.com>
2014-01-02 02:12:27 +0100

    First attempt at exiting efi boot and entering efi runtime.

--- uefi_kernel.c
+++ uefi_kernel.c
@@ -1,14 +1,39 @@
 #include <efi.h>
+#include <efilib.h>
 
-static CHAR16 *exampleText = L"This is HALT!";
+EFI_STATUS load_runtime_image_handler(EFI_HANDLE parentImage, EFI_SYSTEM_TABLE *systab, EFI_HANDLE *handle) {
+  EFI_LOADED_IMAGE *li;
+  EFI_GUID protocol = LOADED_IMAGE_PROTOCOL;
+  if (EFI_ERROR(systab->BootServices->HandleProtocol(parentImage, &protocol, (void**)&li))) {
+    return EFI_LOAD_ERROR;
+  }
+  EFI_DEVICE_PATH *runtime_image_path = FileDevicePath(li->DeviceHandle,  L"\\wat\\wut.efi");
+  return systab->BootServices->LoadImage(FALSE, parentImage, runtime_image_path, NULL, 0, handle);
+}
+
+EFI_STATUS get_memory_map_key(EFI_SYSTEM_TABLE *systab, UINTN *memoryMapKey) {
+  UINTN memoryMapSize;
+  EFI_MEMORY_DESCRIPTOR memoryMap;
+  UINTN memoryMapDescriptorSize;
+  UINT32 memoryMapDescriptorVersion;
+  return systab->BootServices->GetMemoryMap(&memoryMapSize, &memoryMap, memoryMapKey, &memoryMapDescriptorSize, &memoryMapDescriptorVersion);
+}
 
 EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab) {
-  UINTN index;
-  EFI_EVENT event = systab->ConIn->WaitForKey;
   SIMPLE_TEXT_OUTPUT_INTERFACE *conOut = systab->ConOut;
-  conOut->OutputString(conOut, exampleText);
+  conOut->OutputString(conOut, L"This is HALT!");
 
-  systab->BootServices->WaitForEvent(1, &event, &index);
+  UINTN memoryMapKey;
+  if (EFI_ERROR(get_memory_map_key(systab, &memoryMapKey))) {
+    return EFI_LOAD_ERROR;
+  }
+
+  EFI_HANDLE runtimeImageHandler;
+  if (EFI_ERROR(load_runtime_image_handler(image, systab, &runtimeImageHandler))) {
+    return EFI_LOAD_ERROR;
+  }
+
+  systab->BootServices->ExitBootServices(runtimeImageHandler, memoryMapKey);
 
   return EFI_SUCCESS;
 }
