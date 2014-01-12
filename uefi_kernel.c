2d3660aeb12c099985dcf39c148ecfd38725e746
August Lilleaas <august@augustl.com>
2014-01-12 11:55:35 +0100

    Coding style. Underscores > lower camel case.

--- uefi_kernel.c
+++ uefi_kernel.c
@@ -1,46 +1,46 @@
 #include <efi.h>
 #include <efilib.h>
 
-EFI_STATUS get_memory_map_key(EFI_SYSTEM_TABLE *systab, UINTN *memoryMapKey) {
+EFI_STATUS get_memory_map_key(EFI_SYSTEM_TABLE *systab, UINTN *memory_map_key) {
   EFI_STATUS status;
-  UINTN memoryMapSize = 0;
-  EFI_MEMORY_DESCRIPTOR *memoryMap;
-  UINTN memoryMapDescriptorSize;
-  UINT32 memoryMapDescriptorVersion;
+  UINTN memory_map_size = 0;
+  EFI_MEMORY_DESCRIPTOR *memory_map;
+  UINTN memory_map_descriptor_size;
+  UINT32 memory_map_descriptor_version;
 
-  status = systab->BootServices->GetMemoryMap(&memoryMapSize, memoryMap, memoryMapKey, &memoryMapDescriptorSize, &memoryMapDescriptorVersion);
+  status = systab->BootServices->GetMemoryMap(&memory_map_size, memory_map, memory_map_key, &memory_map_descriptor_size, &memory_map_descriptor_version);
   if (status != EFI_BUFFER_TOO_SMALL) {
     return EFI_LOAD_ERROR;
   }
 
   // Spec says we should give it some additional space, since the allocation of the new buffer can
   // potentially increase the memory map size.
-  memoryMapSize *= 1.25;
+  memory_map_size *= 1.25;
 
-  status = systab->BootServices->AllocatePool(EfiLoaderData, memoryMapSize, (void**)&memoryMap);
+  status = systab->BootServices->AllocatePool(EfiLoaderData, memory_map_size, (void**)&memory_map);
   if (status != EFI_SUCCESS) {
-    systab->BootServices->FreePool(memoryMap);
+    systab->BootServices->FreePool(memory_map);
     return status;
   }
 
-  return systab->BootServices->GetMemoryMap(&memoryMapSize, memoryMap, memoryMapKey, &memoryMapDescriptorSize, &memoryMapDescriptorVersion);
+  return systab->BootServices->GetMemoryMap(&memory_map_size, memory_map, memory_map_key, &memory_map_descriptor_size, &memory_map_descriptor_version);
 }
 
 EFI_STATUS efi_main(EFI_HANDLE image, EFI_SYSTEM_TABLE *systab) {
   EFI_STATUS status;
-  SIMPLE_TEXT_OUTPUT_INTERFACE *conOut = systab->ConOut;
-  conOut->OutputString(conOut, L"This is HALT!\r\n");
+  SIMPLE_TEXT_OUTPUT_INTERFACE *con_out = systab->ConOut;
+  con_out->OutputString(con_out, L"This is HALT!\r\n");
 
-  UINTN memoryMapKey;
-  status = get_memory_map_key(systab, &memoryMapKey);
+  UINTN memory_map_key;
+  status = get_memory_map_key(systab, &memory_map_key);
   if (EFI_ERROR(status)) {
-    conOut->OutputString(conOut, L"There was an error allocating memory map information.\r\n");
+    con_out->OutputString(con_out, L"There was an error allocating memory map information.\r\n");
     return status;
   }
 
-  status = systab->BootServices->ExitBootServices(image, memoryMapKey);
+  status = systab->BootServices->ExitBootServices(image, memory_map_key);
   if (EFI_ERROR(status)) {
-    conOut->OutputString(conOut, L"There was an error exiting UEFI boot mode.\r\n");
+    con_out->OutputString(con_out, L"There was an error exiting UEFI boot mode.\r\n");
     return status;
   }
 
