5ca5df4d76983ba39e5e8c7011d8ff5e840b68a1
August Lilleaas <august@augustl.com>
2014-02-16 00:27:16 +0100

    Forgetting to mount is annoying, so we ensure it's mounted.

--- script/_env.sh
+++ script/_env.sh
@@ -1,1 +1,17 @@
 TARGET_DISK=target/virtual_disk.img
+
+is_virtual_disk_mounted() {
+    [ -n "$(mount | grep $(readlink -f $TARGET_DISK))" ] && return 0 || return 1
+}
+
+ensure_mount() {
+    # @if [ -n "$(shell mount | grep "$$(readlink -f $(TARGET_DISK) ) on $$(readlink -f $(MOUNT_DIR))")" ]; then \
+
+    if ( is_virtual_disk_mounted )
+    then
+        continue
+    else
+        echo "Virtual disk not mounted! Run script/mount."
+        exit
+    fi
+}
