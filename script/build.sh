b7641b8bbbe0cc3fd7d96bf9916d2d2cc528b48c
August Lilleaas <august@augustl.com>
2014-09-25 16:46:03 +0200

    GDB specific debug symbols

--- script/build.sh
+++ script/build.sh
@@ -1,6 +1,6 @@
 # TODO: Make gcc location configurable
 BIN=$HOME/local/gcc-cross/bin
-GCCFLAGS="-Wall -Wextra -g -Iinclude"
+GCCFLAGS="-Wall -Wextra -ggdb -Iinclude"
 rm -rf target
 mkdir -p target
 $BIN/i686-elf-as boot/multiboot/halt_multiboot_init.s -o target/halt_multiboot_init.o
