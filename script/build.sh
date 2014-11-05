0029334c58d65421a1fb65545e50dded2992ea05
August Lilleaas <august@augustl.com>
2014-11-05 15:11:42 +0000

    Putting all the cflags in one place.

--- script/build.sh
+++ script/build.sh
@@ -1,10 +1,10 @@
 # TODO: Make gcc location configurable
 BIN=$HOME/local/gcc-cross/bin
 GCCFLAGS_DEBUG="-Og -ggdb"
-GCCFLAGS="-Wall -Wextra -Iinclude $GCCFLAGS_DEBUG"
+GCCFLAGS="-std=gnu99 -nostdlib -ffreestanding -O2 -Wall -Wextra -Iinclude $GCCFLAGS_DEBUG"
 rm -rf target
 mkdir -p target
 $BIN/i686-elf-as boot/multiboot/halt_multiboot_init.s -o target/halt_multiboot_init.o
-$BIN/i686-elf-gcc -c boot/multiboot/halt_multiboot_main.c -o target/halt_multiboot_main.o -std=gnu99 -nostdlib -ffreestanding -O2 -Iboot/multiboot/include $GCCFLAGS
-$BIN/i686-elf-gcc -c kernel/src/halt_main.c -o target/halt_main.o -std=gnu99 -nostdlib -ffreestanding -O2 $GCCFLAGS
-$BIN/i686-elf-gcc -T script/linker.ld -o target/halt_multiboot.bin -ffreestanding -O2 -nostdlib target/halt_multiboot_init.o target/halt_multiboot_main.o target/halt_main.o -lgcc $GCCFLAGS
+$BIN/i686-elf-gcc -c boot/multiboot/halt_multiboot_main.c -o target/halt_multiboot_main.o -Iboot/multiboot/include $GCCFLAGS
+$BIN/i686-elf-gcc -c kernel/src/halt_main.c -o target/halt_main.o $GCCFLAGS
+$BIN/i686-elf-gcc -T script/linker.ld -o target/halt_multiboot.bin target/halt_multiboot_init.o target/halt_multiboot_main.o target/halt_main.o -lgcc $GCCFLAGS
