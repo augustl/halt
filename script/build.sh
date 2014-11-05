652951723b7f50e99b21c7b584ec298c5b55c739
August Lilleaas <august@augustl.com>
2014-11-05 15:06:40 +0000

    Removing objcopy steps, we don't need 'em.

--- script/build.sh
+++ script/build.sh
@@ -8,5 +8,3 @@ $BIN/i686-elf-as boot/multiboot/halt_multiboot_init.s -o target/halt_multiboot_i
 $BIN/i686-elf-gcc -c boot/multiboot/halt_multiboot_main.c -o target/halt_multiboot_main.o -std=gnu99 -nostdlib -ffreestanding -O2 -Iboot/multiboot/include $GCCFLAGS
 $BIN/i686-elf-gcc -c kernel/src/halt_main.c -o target/halt_main.o -std=gnu99 -nostdlib -ffreestanding -O2 $GCCFLAGS
 $BIN/i686-elf-gcc -T script/linker.ld -o target/halt_multiboot.bin -ffreestanding -O2 -nostdlib target/halt_multiboot_init.o target/halt_multiboot_main.o target/halt_main.o -lgcc $GCCFLAGS
-$BIN/i686-elf-objcopy --only-keep-debug target/halt_multiboot.bin target/halt_multiboot.sym
-$BIN/i686-elf-objcopy --strip-debug target/halt_multiboot.bin
