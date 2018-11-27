647b9542ee43b1e24390886b23e09d6e675c418a
August Lilleaas <august@augustl.com>
2018-11-27 19:22:18 +0100

    Add plain main

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+cargo rustc -- -Z pre-link-arg=-nostartfiles
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
